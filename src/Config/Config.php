987b4e8e357c6617b5bed8dbeb6432c6c051fc47
Everton da Rosa <everton3x@gmail.com>
2020-05-16 12:15:52 -0300

    Added ConfigLoader and Config classes

---
+++ src/Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Meduza\Config;
+
+/**
+ * The meduza configuration
+ *
+ * @author everton
+ */
+class Config {
+    //put your code here
+}
---
+++ src/Config/ConfigLoader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Meduza\Config;
+
+/**
+ * Loader for config files
+ *
+ * @author everton
+ */
+class ConfigLoader {
+    //put your code here
+}
